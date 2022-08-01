2066630032d31fb2e87d7d1a60ea915515818336
host4ideas <felixmb99@hotmail.es>
2022-08-01 14:11:39 +0200

    developing errorcomponent

---
+++ src/components/ErrorComponent.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      message: "",
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <h1>Error</h1>
+  </div>
+</template>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  display: block;
+  margin: auto;
+  text-align: center;
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+  width: 50%;
+}
+</style>
