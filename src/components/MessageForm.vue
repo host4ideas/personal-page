046591b34a64b28a67ee5e54b42c870906c13f9a
host4ideas <72515482+host4ideas@users.noreply.github.com>
2022-10-04 12:46:22 +0200

    Revert "added theme animations"
    This reverts commit be779e061b5cb94ac3a87952d137d2b27ae86872.

--- src/components/MessageForm.vue
+++ src/components/MessageForm.vue
@@ -1,163 +1,124 @@
 <script>
-import Popup from "./Popup.vue";
-import LoadingIcon from "./LoadingIcon.vue";
+import nodemailer from "nodemailer";
+const { EMAIL, EMAIL_PASSWORD } = import.meta.env;
 
 export default {
-    components: {
-        Popup,
-        LoadingIcon,
+  data() {
+    return {
+      name: "",
+      email: "",
+      message: "",
+    };
+  },
+  methods: {
+    async sendEmail(e) {
+      try {
+        // create reusable transporter object using the default SMTP transport
+        var transporter = nodemailer.createTransport({
+          service: "Gmail",
+          auth: {
+            user: "elsemasturba@gmail.com",
+            pass: "2cuentaparabrawlstars",
+          },
+        });
+
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+          from: `${this.name} <${this.email}>`, // sender address
+          to: EMAIL, // list of receivers
+          subject: "Contact me", // Subject line
+          text: this.message, // plain text body
+          html: `<b>${this.message}</b>`, // html body
+        });
+
+        console.log("Message sent: %s", info.messageId);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+      } catch (error) {
+        console.log({ error });
+      }
+
+      // Reset form field
+      this.name = "";
+      this.email = "";
+      this.message = "";
     },
-    data() {
-        return {
-            name: "",
-            email: "",
-            subject: "",
-            message: "",
-            popupType: "",
-            popupTitle: "",
-            popupDescription: "",
-            showPopup: false,
-            loading: false,
-        };
-    },
-    methods: {
-        resetForm() {
-            this.name = "";
-            this.email = "";
-            this.subject = "";
-            this.message = "";
-            this.popupType = "";
-            this.popupTitle = "";
-            this.popupDescription = "";
-            this.loading = false;
-        },
-        sendEmail() {
-            this.loading = true;
-            fetch("/api/sendEmail", {
-                method: "POST",
-                body: JSON.stringify({
-                    name: this.name,
-                    email: this.email,
-                    subject: this.subject,
-                    message: this.message,
-                }),
-            })
-                .then((response) => response.json())
-                .then((mailerResponse) => {
-                    if (mailerResponse.status !== 200) {
-                        // Email failed -> show error Popup component
-                        this.popupType = "error";
-                        this.popupTitle = "An error has ocurred.";
-                        this.popupDescription = mailerResponse.json();
-                        this.showPopup = true;
-                        this.resetForm();
-                    } else {
-                        // Email failed -> show successful Popup component
-                        this.popupType = "success";
-                        this.popupTitle = "Message was send successfully!!!";
-                        this.popupDescription = mailerResponse.json();
-                        this.showPopup = true;
-                        this.resetForm();
-                    }
-                });
-        },
-    },
+  },
+  mounted() {
+    this.$refs.p.textContent = "mounted!";
+  },
 };
 </script>
 
 <template>
-    <div class="container smooth-theme">
-        <div v-if="showPopup">
-            <Popup
-                :type="popupType"
-                :title="popupTitle"
-                :description="popupDescription"
-            />
-        </div>
-        <form v-on:submit.prevent="sendEmail">
-            <label class="smooth-theme">Name</label>
-            <input
-                autofocus
-                type="text"
-                v-model="name"
-                name="name"
-                placeholder="Your Name"
-            />
-            <label class="smooth-theme">Email</label>
-            <input
-                type="email"
-                v-model="email"
-                name="email"
-                placeholder="Your Email"
-            />
-            <label class="smooth-theme">Subject</label>
-            <input
-                type="text"
-                name="subject"
-                v-model="subject"
-                placeholder="Subject"
-            />
-            <label class="smooth-theme">Message</label>
-            <textarea
-                name="message"
-                v-model="message"
-                cols="30"
-                rows="5"
-                placeholder="Message"
-            >
-            </textarea>
+  <div class="container">
+    <p ref="p">hello</p>
+    <form v-on:submit.prevent="sendEmail">
+      <label>Name</label>
+      <input type="text" v-model="name" name="name" placeholder="Your Name" />
+      <label>Email</label>
+      <input
+        type="email"
+        v-model="email"
+        name="email"
+        placeholder="Your Email"
+      />
+      <label>Message</label>
+      <textarea
+        name="message"
+        v-model="message"
+        cols="30"
+        rows="5"
+        placeholder="Message"
+      >
+      </textarea>
 
-            <button type="submit" class="smooth-theme">
-                <div v-if="loading"><LoadingIcon /></div>
-                <div v-else>Send</div>
-            </button>
-        </form>
-    </div>
+      <input type="submit" value="Send" />
+    </form>
+  </div>
 </template>
 
-<style>
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+
 .container {
-    display: block;
-    margin: auto;
-    text-align: center;
-    border-radius: 5px;
-    background-color: var(--message-bg);
-    padding: 20px;
-    width: 50%;
+  display: block;
+  margin: auto;
+  text-align: center;
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+  width: 50%;
 }
 
 label {
-    float: left;
-    color: var(--font-color);
+  float: left;
 }
 
 input[type="text"],
 [type="email"],
 textarea {
-    width: 100%;
-    padding: 12px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    box-sizing: border-box;
-    margin-top: 6px;
-    margin-bottom: 16px;
-    resize: vertical;
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
 }
 
-button[type="submit"] {
-    background-color: var(--send-button);
-    color: white;
-    padding: 12px 20px;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-    min-width: 75px;
-    font-weight: 600;
-    font-size: 1rem;
-    color: rgba(231, 246, 242, 1);
+input[type="submit"] {
+  background-color: #4caf50;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
 }
 
-button[type="submit"]:hover {
-    background-color: var(--send-button-hover);
+input[type="submit"]:hover {
+  background-color: #45a049;
 }
 </style>
